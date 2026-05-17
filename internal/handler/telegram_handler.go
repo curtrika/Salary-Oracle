9f81c27739910bcef19f3a9a9a971eb6eafdcdb6
curtrika <chereshnev.n.s@gmail.com>
2026-05-17 08:53:52 +0300

    added basic telegram handler func

--- internal/handler/telegram_handler.go
+++ internal/handler/telegram_handler.go
@@ -1,1 +1,41 @@
 package handler
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/go-telegram/bot"
+	"github.com/go-telegram/bot/models"
+)
+
+type TelegramHandler struct {
+	bot *bot.Bot
+}
+
+func NewTelegramHandler(b *bot.Bot) *TelegramHandler {
+	return &TelegramHandler{
+		bot: b,
+	}
+}
+
+func (h *TelegramHandler) RegisterHandlers() {
+	h.bot.RegisterHandler(bot.HandlerTypeMessageText, "/start", bot.MatchTypeExact, h.handleStart)
+
+	h.bot.RegisterHandler(bot.HandlerTypeMessageText, "/ping", bot.MatchTypePrefix, h.handlePing)
+}
+
+func (h *TelegramHandler) handleStart(ctx context.Context, b *bot.Bot, update *models.Update) {
+	msg := "👋 Привет!"
+
+	b.SendMessage(ctx, &bot.SendMessageParams{
+		ChatID: update.Message.Chat.ID,
+		Text:   msg,
+	})
+}
+
+func (h *TelegramHandler) handlePing(ctx context.Context, b *bot.Bot, update *models.Update) {
+	b.SendMessage(ctx, &bot.SendMessageParams{
+		ChatID: update.Message.Chat.ID,
+		Text:   fmt.Sprintf("pong", update.Message.Text),
+	})
+}
